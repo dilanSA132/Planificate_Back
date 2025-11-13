a78c68d761360c7916839d16c45af68c62960bf4
elmancho0 <granadosgabriel34@gmail.com>
2025-11-12 18:21:15 -0600

    [FIX] Load environment variables from .env file to configure database connection

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
